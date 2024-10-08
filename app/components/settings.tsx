e1eae965b83466d695ee5cd7e09804ea036366c2
saika <31368587+huzhongcaijia@users.noreply.github.com>
2024-10-09 01:36:19 +0800

    remove ads

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -688,7 +688,7 @@ export function Settings() {
     </ListItem>
   );
 
-  const saasStartComponent = (
+  /* const saasStartComponent = (
     <ListItem
       className={styles["subtitle-button"]}
       title={
@@ -711,7 +711,7 @@ export function Settings() {
         }}
       />
     </ListItem>
-  );
+  ); */
 
   const useCustomConfigComponent = // Conditionally render the following ListItem based on clientConfig.isApp
     !clientConfig?.isApp && ( // only show if isApp is false
@@ -1585,7 +1585,7 @@ export function Settings() {
         </List>
 
         <List id={SlotID.CustomModel}>
-          {saasStartComponent}
+          {/* {saasStartComponent} */}
           {accessCodeComponent}
 
           {!accessStore.hideUserApiKey && (
